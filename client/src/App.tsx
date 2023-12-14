49bb33b2e6b281c6b8deec4d3b9d945a6398214a
shstprvvv <shestoperov01@gmail.com>
2023-12-14 14:59:20 +0300

    done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+
 import { Route, Routes } from 'react-router-dom';
-import type { AxiosError } from 'axios';
-import { useAppDispatch, useAppSelector } from './redux/hook';
-import NavBar from './components/NavBar';
-import PrivateRouter from './components/PrivateRouter/PrivateRouter';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
-// import NotePage from './pages/ProductPage';
-import { checkUserThunk, refreshThunk } from './redux/slices/auth/authThunks';
+import { ChakraProvider } from '@chakra-ui/react';
+import LoginFormModal from './forms/LoginFormModal';
 import MainPage from './pages/MainPage';
-import { apiNotesService } from './services/notes';
-import Loader from './components/Loader';
-import { thunkNotesLoad } from './redux/slices/notes/createAsyncThunk';
-import Favourites from './pages/FavouritesPage';
+import SideBar from './components/SideBar';
+import TeacherAccountPage from './pages/TeacherAccountPage';
 
 function App(): JSX.Element {
   return (
-    <Loader isLoading={auth === 'pending'}>
-    <>
-      <NavBar />
+    <ChakraProvider>
+      <SideBar />
+      <div>123456789</div>
       <Routes>
-        <>
-          <Route path="/" element={<MainPage />} />
-          <Route
-            element={
-              <PrivateRouter isAllowed={auth.user.status === 'authenticated'} redirectPath="/" />
-            }
-          >
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<RegistrationPage />} />
-          </Route>
-          <Route
-            element={
-              <PrivateRouter
-                isAllowed={auth.user.status !== 'authenticated'}
-                redirectPath="/login"
-              />
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/favourites" element={<Favourites />} />
-        </>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginFormModal />} />
+        <Route path="/teacherlk" element={<TeacherAccountPage />} />
       </Routes>
-    </>
-     </Loader>
+    </ChakraProvider>
+    
   );
 }
 
