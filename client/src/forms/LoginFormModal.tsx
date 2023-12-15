7cdd4896594c15fa2ddd757998c2b15012eac6c1
Irina Dyachenko <kulagina.iraa@yandex.ru>
2023-12-15 15:07:23 +0300

    login modal

--- client/src/forms/LoginFormModal.tsx
+++ client/src/forms/LoginFormModal.tsx
@@ -13,58 +13,52 @@ import {
   Input,
 } from '@chakra-ui/react';
 import React from 'react';
-import { useAppDispatch } from '../redux/hook';
+import { useAppDispatch, useAppSelector } from '../redux/hook';
+import { closeModal, toggleModal } from '../redux/slices/modal/modalReducer';
+import { thunkLogin } from '../redux/slices/auth/createAsyncThunks';
+import type { LoginFormData } from '../types/auth';
 
 export default function LoginFormModal(): JSX.Element {
   const dispatch = useAppDispatch();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const show = useAppSelector((store) => store.modal.isOpen);
 
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
+  // const initialRef = React.useRef(null);
+  // const finalRef = React.useRef(null);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        const formData = Object.fromEntries(new FormData(e.currentTarget)) as LoginFormData;
-        void dispatch(thunkAuthLogin(formData));
-      }}
-    >
-      <Button onClick={onOpen}>Open Modal</Button>
-      <Button ml={4} ref={finalRef}>
-        I'll receive focus on close
-      </Button>
-
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
+    <Modal onClose={() => dispatch(closeModal())} isOpen={show}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create your account</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const formData = Object.fromEntries(new FormData(e.currentTarget)) as LoginFormData;
+              void dispatch(thunkLogin(formData));
+              dispatch(closeModal());
+            }}
+          >
             <FormControl>
               <FormLabel>Email</FormLabel>
-              <Input ref={initialRef} name="email" type="text" placeholder="Почта" />
+              <Input name="email" type="text" placeholder="Почта" />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Пароль</FormLabel>
               <Input name="password" type="text" placeholder="Почта" />
             </FormControl>
-          </ModalBody>
+          </form>
+        </ModalBody>
 
-          <ModalFooter>
-            <Button type="submit" colorScheme="blue" mr={3}>
-              Войти
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </form>
+        <ModalFooter>
+          <Button type="submit" colorScheme="blue" mr={3}>
+            Войти
+          </Button>
+          <Button onClick={() => dispatch(closeModal())}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 }
