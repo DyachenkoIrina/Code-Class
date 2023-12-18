02186b0786073c7103e943ace14c79085cf3634b
nat <natalbka.p92@gmail.com>
2023-12-18 12:35:29 +0300

    db changed

--- server/src/routes/teacherRouter.js
+++ server/src/routes/teacherRouter.js
@@ -1,32 +1,36 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const { Teacher } = require("../../db/models");
-const generateTokens = require("../utils/generateTokens");
-const jwtConfig = require("../config/jwtConfig");
-const cookiesConfig = require("../config/cookiesConfig");
+const { User } = require("../../db/models");
+const { Group , Teacher, TeacherGroup} = require("../../db/models");
 
 const teacherRouter = express.Router();
 
 // teacherRouter.get("/", async (req, res) => {
 //   try {
-//     const data = await Teacher.findAll();
-//     console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', data)
+//     const data = await Group.findAll({where: });
+//     console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", data);
 //     res.status(200).json(data);
 //   } catch ({ message }) {
 //     res.status(400).json({ message });
 //   }
 // });
 
-// teacherRouter.get("/students/:id", async (req, res) => {
-//   try {
-//     const data = await User.findAll({
-//       where: { role: "Student", groupId: req.params.id },
-//     });
-//     res.status(200).json(data);
-//   } catch ({ message }) {
-//     res.status(400).json({ message });
-//   }
-// });
+// teacherRouter.get("/:id", async (req, res) => {
+//     try {
+//   const teacher = await Teacher.findByPk(req.body.id)
+//   const groupsTeasher= await TeacherGroup.findAll({where:{t}})
+//     }
+//   });
+
+teacherRouter.get("/students/:id", async (req, res) => {
+  try {
+    const data = await User.findAll({
+      where: { role: "Student", groupId: req.params.id },
+    });
+    res.status(200).json(data);
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
 
 // teacherRouter.get("/studentid/:id", async (req, res) => {
 //   const { id } = req.params;
