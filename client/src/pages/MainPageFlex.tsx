91fee4248ef30f8fa9f78ba3de81a880886eac4d
nat <natalbka.p92@gmail.com>
2023-12-20 14:17:01 +0300

    rel5.6

--- client/src/pages/MainPageFlex.tsx
+++ client/src/pages/MainPageFlex.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Container, Flex, Box, Image } from '@chakra-ui/react';
-import { useAppDispatch, useAppSelector } from '../redux/hook';
 import ModalFromRegistration from '../components/FormFromRegistration';
 import YandexMap from '../components/YandexMap';
 import CourseOptions from '../components/CourseOptions';
 import CarouselImg from '../components/Carousel';
-import { toggleModal } from '../redux/slices/modal/modalReducer';
 import HeadingComponent from '../components/Heading';
 import Video from '../components/Video';
-
 import '../index.css';
 
 export default function MainPageFlex(): JSX.Element {
-  const dispatch = useAppDispatch();
-  const modalState = useAppSelector((state) => state.modal.registrModal);
 
-  const handleToggleModal = (): void => {
-    dispatch(toggleModal());
-  };
 
   return (
     <Flex flexDirection="column">
@@ -35,9 +27,7 @@ export default function MainPageFlex(): JSX.Element {
             <Container class="heading">
               <HeadingComponent class="heading_text" />
               <ModalFromRegistration
-                class="heading_btn"
-                isOpen={modalState}
-                onClose={handleToggleModal}
+              
               />
             </Container>
             <CarouselImg />
